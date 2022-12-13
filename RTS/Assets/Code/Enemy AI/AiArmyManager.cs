a8837f92e3d38d010e14540e1fdd9b34806c7ba5
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-12-13 17:15:41 +0100

    Ai builds units

--- RTS/Assets/Code/Enemy AI/AiArmyManager.cs
+++ RTS/Assets/Code/Enemy AI/AiArmyManager.cs
@@ -25,13 +25,17 @@ public class AiArmyManager : MonoBehaviour
     public List<SupplySet> supplySets = new List<SupplySet>();
     public List<SupplyTruck> supplyTrucks = new List<SupplyTruck>();
     public List<Factory> lFactories = new List<Factory>();
+    int l = 0;
     public List<Factory> hFactories = new List<Factory>();
+    int h = 0;
+    public List<ActiveSquad> activeSquads = new List<ActiveSquad>();
 
     [Header("Army production")]
     public ArmySquads squads;
     public int squadCap;
     public Squad squadToBuild;
     public bool isBuilding;
+    public Transform gatheringPoint;
 
     private void Start()
     {
@@ -68,7 +72,7 @@ public class AiArmyManager : MonoBehaviour
         squadCap = 3;
         for (int i = squadCap; i < squads.squads.Length; i++)
         {
-            yield return new WaitForSeconds((11f - agroRating) * 90f);
+            yield return new WaitForSeconds((11f - agroRating) * 30f + 60f);
             ecoRating = Mathf.Clamp(ecoRating + Random.Range(0.1f, 0.75f), 1, 10);
             squadCap++;
             isBuilding = false;
@@ -78,22 +82,110 @@ public class AiArmyManager : MonoBehaviour
 
     void SetNewSquad()
     {
+        isBuilding = true;
         int i = Random.Range(0, squadCap);
         squadToBuild = new Squad(squads.squads[i]);
-        isBuilding = true;
+        StartCoroutine(BuildNewSquad());
+    }
+
+    IEnumerator BuildNewSquad()
+    {
+        activeSquads.Add(new ActiveSquad());
+        while (squadToBuild.humvees + squadToBuild.apcs + squadToBuild.tanks + squadToBuild.howitzers + squadToBuild.snipers > 0)
+        {
+            for (int i = 0; i < squadToBuild.humvees; i++)
+            {
+                if (HQBuilding.GetSupplies(lFactories[0].troops[0].cost, army))
+                {
+                    if (l >= lFactories.Count)
+                        l = 0;
+                    lFactories[l].BuildNewTroop(0);
+                    l++;
+                    squadToBuild.humvees--;
+                    i--;
+                }
+            }
+            for (int i = 0; i < squadToBuild.apcs; i++)
+            {
+                if (HQBuilding.GetSupplies(lFactories[0].troops[1].cost, army))
+                {
+                    if (l >= lFactories.Count)
+                        l = 0;
+                    lFactories[l].BuildNewTroop(1);
+                    l++;
+                    squadToBuild.apcs--;
+                    i--;
+                }
+            }
+            for (int i = 0; i < squadToBuild.tanks; i++)
+            {
+                if (HQBuilding.GetSupplies(hFactories[0].troops[0].cost, army))
+                {
+                    if (h >= lFactories.Count)
+                        h = 0;
+                    hFactories[h].BuildNewTroop(0);
+                    h++;
+                    squadToBuild.tanks--;
+                    i--;
+                }
+            }
+            for (int i = 0; i < squadToBuild.howitzers; i++)
+            {
+                if (HQBuilding.GetSupplies(hFactories[0].troops[1].cost, army))
+                {
+                    if (h >= lFactories.Count)
+                        h = 0;
+                    hFactories[h].BuildNewTroop(1);
+                    h++;
+                    squadToBuild.howitzers--;
+                    i--;
+                }
+            }
+            for (int i = 0; i < squadToBuild.snipers; i++)
+            {
+                if (HQBuilding.GetSupplies(hFactories[0].troops[2].cost, army))
+                {
+                    if (h >= lFactories.Count)
+                        h = 0;
+                    hFactories[h].BuildNewTroop(2);
+                    h++;
+                    squadToBuild.snipers--;
+                    i--;
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        while (CheckIfBuilding())
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        isBuilding = false;
     }
 
     private void FixedUpdate()
     {
         CheckCurrentEco();
-        if (CurrentEcoScore() < ecoRating / 10)
+        CheckSquads();
+        if (CurrentEcoScore() < ecoRating / 10f)
         {
             if (HQBuilding.GetSupplies(hq.troops[0].cost, army))
                 hq.BuildNewTroop(0);
         }
         else if (!isBuilding)
         {
+            isBuilding = true;
             SetNewSquad();
+        }
+        foreach (UnitBase unit in PlayerTroopManager.instance.allUnits)
+        {
+            if (unit.army == army && unit.GetComponent<TroopMovement>())
+            {
+                if (CheckIfAssigned(unit) == false)
+                {
+                    activeSquads[activeSquads.Count - 1].squadMemebers.Add(unit.GetComponent<TroopMovement>());
+                    Formations.instance.SetFormation(activeSquads[activeSquads.Count - 1].squadMemebers.ToArray(), gatheringPoint.position);
+                }
+            }
         }
     }
     void CheckCurrentEco()
@@ -138,6 +230,19 @@ public class AiArmyManager : MonoBehaviour
             }
         }
     }
+    void CheckSquads()
+    {
+        for (int i = activeSquads.Count - 1; i >= 0; i--)
+        {
+            for (int u = activeSquads[i].squadMemebers.Count - 1; u >= 0; u--)
+            {
+                if (activeSquads[i].squadMemebers[u] == null)
+                    activeSquads[i].squadMemebers.RemoveAt(u);
+            }
+            if (activeSquads[i].squadMemebers.Count == 0 && i != activeSquads.Count - 1)
+                activeSquads.RemoveAt(i);
+        }
+    }
     float CurrentEcoScore()
     {
         int eco = 0;
@@ -164,6 +269,36 @@ public class AiArmyManager : MonoBehaviour
         Debug.Log(eco + ", " + optimalEco + ", " + ecoScore.ToString());
         return ecoScore;
     }
+    bool CheckIfBuilding()
+    {
+        for (int i = 0; i < lFactories.Count; i++)
+        {
+            if (lFactories[i].isBuilding)
+                return true;
+        }
+        for (int i = 0; i < hFactories.Count; i++)
+        {
+            if (hFactories[i].isBuilding)
+                return true;
+        }
+        return false;
+    }
+    bool CheckIfAssigned(UnitBase unit)
+    {
+        TroopMovement troop;
+        if (unit.GetComponent<SupplyTruck>())
+            return true;
+        else
+        {
+            troop = unit.GetComponent<TroopMovement>();
+            for (int i = 0; i < activeSquads.Count; i++)
+            {
+                if (activeSquads[i].squadMemebers.Contains(troop))
+                    return true;
+            }
+        }
+        return false;
+    }    
 }
 
 [System.Serializable]
@@ -172,4 +307,10 @@ public class SupplySet
     public SupplyYard yard;
     public SupplyDepot depot;
     public int optimalEco;
+}
+
+[System.Serializable]
+public class ActiveSquad
+{
+    public List<TroopMovement> squadMemebers = new List<TroopMovement>();
 }
